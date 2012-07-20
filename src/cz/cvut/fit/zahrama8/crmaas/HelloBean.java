97ef2d17c7b90e287eab2f8446353be126e42117
martin <martin@ubuntu.ubuntu-domain>
2012-07-20 17:14:24 +0200

    init commit

---
+++ src/cz/cvut/fit/zahrama8/crmaas/HelloBean.java
@@ -0,0 +1,23 @@
+package cz.cvut.fit.zahrama8.crmaas;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class HelloBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
